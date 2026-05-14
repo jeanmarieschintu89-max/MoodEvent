59fde9e17b2f5c7ce82631b208f192ef09704ad6
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-14 20:30:57 +0200

    plugin: register event gui and chat listeners

--- src/main/java/fr/moodcraft/event/Main.java
+++ src/main/java/fr/moodcraft/event/Main.java
@@ -2,8 +2,11 @@ package fr.moodcraft.event;
 
 import fr.moodcraft.event.command.EventAdminCommand;
 import fr.moodcraft.event.command.EventCommand;
+import fr.moodcraft.event.listener.EventAdminGUIListener;
+import fr.moodcraft.event.listener.EventChatListener;
 import fr.moodcraft.event.manager.EventManager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -28,6 +31,7 @@ public class Main extends JavaPlugin {
         registerCommand("event", eventCommand);
 
         registerCommand("eventadmin", adminCommand);
+        registerCommand("eventgui", adminCommand);
         registerCommand("eventcreate", adminCommand);
         registerCommand("eventdesc", adminCommand);
         registerCommand("eventtype", adminCommand);
@@ -38,8 +42,12 @@ public class Main extends JavaPlugin {
         registerCommand("eventstop", adminCommand);
         registerCommand("eventcancel", adminCommand);
 
+        Bukkit.getPluginManager().registerEvents(new EventAdminGUIListener(), this);
+        Bukkit.getPluginManager().registerEvents(new EventChatListener(), this);
+
         getLogger().info("=================================");
         getLogger().info("✦ MoodEvent activé");
+        getLogger().info("Centre événementiel chargé");
         getLogger().info("File d'attente événementielle prête");
         getLogger().info("=================================");
     }
