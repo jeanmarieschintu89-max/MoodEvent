9a938848e80739d188b0581f8a3cc0f29477e9a5
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-15 13:34:04 +0200

    Ajoute la detection automatique de l'arrivee

---
+++ src/main/java/fr/moodcraft/event/listener/EventProgressListener.java
@@ -0,0 +1,35 @@
+package fr.moodcraft.event.listener;
+
+import fr.moodcraft.event.manager.EventManager;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class EventProgressListener implements Listener {
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        if (!hasChangedBlock(event.getFrom(), event.getTo())) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        if (!EventManager.isAtFinish(player)) {
+            return;
+        }
+
+        EventManager.finishPlayer(player);
+    }
+
+    private boolean hasChangedBlock(Location from, Location to) {
+        if (to == null || from.getWorld() == null || to.getWorld() == null) {
+            return false;
+        }
+        return from.getBlockX() != to.getBlockX()
+                || from.getBlockY() != to.getBlockY()
+                || from.getBlockZ() != to.getBlockZ()
+                || !from.getWorld().equals(to.getWorld());
+    }
+}
