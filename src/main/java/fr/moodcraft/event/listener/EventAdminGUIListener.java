29d7a5bad315c163d1251487e4552db90a27d94c
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-15 13:11:52 +0200

    Branche le menu recompenses

--- src/main/java/fr/moodcraft/event/listener/EventAdminGUIListener.java
+++ src/main/java/fr/moodcraft/event/listener/EventAdminGUIListener.java
@@ -1,7 +1,10 @@
 package fr.moodcraft.event.listener;
 
 import fr.moodcraft.event.gui.EventAdminGUI;
+import fr.moodcraft.event.gui.RewardGUI;
 import fr.moodcraft.event.manager.EventManager;
+import fr.moodcraft.event.manager.RewardManager;
+import fr.moodcraft.event.manager.WaitingRoomManager;
 import fr.moodcraft.event.util.MoodStyle;
 
 import org.bukkit.Sound;
@@ -9,53 +12,88 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 
 public class EventAdminGUIListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent event) {
 
-        if (!isEventAdminTitle(event.getView().getTitle())) {
-            return;
-        }
-
-        event.setCancelled(true);
-
         if (!(event.getWhoClicked() instanceof Player player)) {
             return;
         }
 
-        int slot = event.getRawSlot();
+        String title = MoodStyle.cleanTitle(event.getView().getTitle());
 
+        if (title.equals("centre evenementiel")) {
+            handleAdminClick(event, player);
+            return;
+        }
+
+        if (title.equals("recompenses event")) {
+            handleRewardClick(event, player);
+        }
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent event) {
+        if (!(event.getPlayer() instanceof Player player)) {
+            return;
+        }
+
+        if (!RewardManager.isEditingItems(player)) {
+            return;
+        }
+
+        RewardManager.saveItemEditor(player, event.getInventory());
+    }
+
+    private void handleAdminClick(InventoryClickEvent event, Player player) {
+        event.setCancelled(true);
+
+        int slot = event.getRawSlot();
         if (slot < 0 || slot >= event.getView().getTopInventory().getSize()) {
             return;
         }
 
         switch (slot) {
-
             case 10 -> {
                 click(player);
                 EventChatListener.startName(player);
             }
-
             case 12 -> {
                 click(player);
                 EventChatListener.startDescription(player);
             }
-
             case 14 -> {
                 click(player);
                 EventManager.cycleType(player);
                 EventAdminGUI.open(player);
             }
-
             case 16 -> {
                 click(player);
                 EventManager.setLocation(player);
                 EventAdminGUI.open(player);
             }
-
+            case 18 -> {
+                click(player);
+                EventManager.setFinishLocation(player);
+                EventAdminGUI.open(player);
+            }
             case 20 -> {
+                click(player);
+                if (WaitingRoomManager.hasRoom()) {
+                    WaitingRoomManager.teleport(player);
+                } else {
+                    WaitingRoomManager.build(player, "medium");
+                }
+                EventAdminGUI.open(player);
+            }
+            case 29 -> {
+                click(player);
+                RewardGUI.open(player);
+            }
+            case 31 -> {
                 click(player);
                 if (EventManager.isQueueOpen()) {
                     EventManager.closeQueue(player);
@@ -64,37 +102,66 @@ public class EventAdminGUIListener implements Listener {
                 }
                 EventAdminGUI.open(player);
             }
-
-            case 22 -> {
+            case 33 -> {
                 click(player);
                 player.closeInventory();
                 EventManager.startEvent(player);
             }
-
-            case 24 -> {
+            case 35 -> {
                 click(player);
                 player.closeInventory();
                 EventManager.stopEvent(player);
             }
-
-            case 26 -> {
+            case 38 -> {
+                player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 0.85f);
+                WaitingRoomManager.restore(player);
+                EventAdminGUI.open(player);
+            }
+            case 42 -> {
                 player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 0.85f);
                 player.closeInventory();
                 EventManager.cancelEvent(player);
             }
-
-            case 40 -> {
+            case 44 -> {
                 player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 0.8f, 1.2f);
                 player.closeInventory();
             }
-
             default -> {
             }
         }
     }
 
-    private boolean isEventAdminTitle(String title) {
-        return MoodStyle.cleanTitle(title).equals("centre evenementiel");
+    private void handleRewardClick(InventoryClickEvent event, Player player) {
+        event.setCancelled(true);
+
+        int slot = event.getRawSlot();
+        if (slot < 0 || slot >= event.getView().getTopInventory().getSize()) {
+            return;
+        }
+
+        switch (slot) {
+            case 11 -> openItems(player, RewardManager.PARTICIPATION);
+            case 12 -> editMoney(player, RewardManager.PARTICIPATION);
+            case 20 -> openItems(player, 1);
+            case 21 -> editMoney(player, 1);
+            case 22 -> openItems(player, 2);
+            case 23 -> editMoney(player, 2);
+            case 24 -> openItems(player, 3);
+            case 25 -> editMoney(player, 3);
+            case 34 -> player.closeInventory();
+            default -> {
+            }
+        }
+    }
+
+    private void openItems(Player player, int place) {
+        click(player);
+        RewardManager.openItemEditor(player, place);
+    }
+
+    private void editMoney(Player player, int place) {
+        click(player);
+        RewardManager.startMoneyInput(player, place);
     }
 
     private void click(Player player) {
