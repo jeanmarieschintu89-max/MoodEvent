efe2aa47895dfa594b8d0d876121161f2ac47368
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-17 01:32:37 +0200

    Add labyrinth event type

--- src/main/java/fr/moodcraft/event/model/EventType.java
+++ src/main/java/fr/moodcraft/event/model/EventType.java
@@ -5,6 +5,7 @@ public enum EventType {
     SURVIE_ETAGES("§dTour Infernale", "§d▣"),
     RUEE_OR("§6Mine en folie", "§6⛏"),
     WATER_JUMP("§bWater Jump", "§b≈"),
+    LABYRINTHE("§aLabyrinthe", "§a⌘"),
     CUSTOM("§fÉvénement libre", "§f✦");
 
     private final String displayName;
@@ -24,7 +25,7 @@ public enum EventType {
     }
 
     public boolean usesFinishLine() {
-        return this == WATER_JUMP;
+        return this == WATER_JUMP || this == LABYRINTHE;
     }
 
     public boolean usesSurvivalRanking() {
@@ -51,6 +52,7 @@ public enum EventType {
             case "tour_infernale", "effondrement", "survie_etages", "survie_des_etages", "etages", "etage", "floor", "floors", "floor_survival" -> SURVIE_ETAGES;
             case "mine_en_folie", "mine_folie", "ruee_or", "ruee_vers_lor", "ruee_vers_or", "or", "gold", "gold_rush", "mine", "minage", "mining" -> RUEE_OR;
             case "waterjump", "water_jump", "water", "eau", "jump_eau", "saut_eau", "water_jumps" -> WATER_JUMP;
+            case "labyrinthe", "laby", "maze", "mazes", "labyrinth", "dedale", "dédale" -> LABYRINTHE;
             default -> CUSTOM;
         };
     }
